b0bd2acae6740cc2c669b51677aa9b1793e94599
Dylan Skokan <36804815+DylanSkokan@users.noreply.github.com>
2022-06-04 12:55:03 -0500

    Delete accountCreation.py

--- accountCreation.py
+++
@@ -1,9 +0,0 @@
-import accountInfo
-from accountInfo import AccountData
-
-acc1 = AccountData(1, "Dylan", "password")
-acc1 = AccountData(6, "Mike", "password")
-acc1 = AccountData(3, "Jerry", "password")
-
-print(*accountInfo.accountList)
-
